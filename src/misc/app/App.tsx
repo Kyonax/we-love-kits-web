dc6e1ff78cb5b7b0042d910fd6ee1aecbfbeee2b
Kyonax <kyonax.corp@gmail.com>
2023-10-19 07:25:02 -0500

    New Modals Adding System

--- src/misc/app/App.tsx
+++ src/misc/app/App.tsx
@@ -95,11 +95,11 @@ function App() {
 
             <header> {navBarRender()} </header>
 
-            <Routes> <Route path='/testing-we-love-kits-v45-24fps' element={firstSectionRender()} /> </Routes>
+            <Routes> <Route path='/testing-we-love-kits-v46-24fps' element={firstSectionRender()} /> </Routes>
 
             <div className='text-white'>
                 <div>
-                    <Routes> <Route path='/testing-we-love-kits-v45-24fps' element={landingKitsRender()} /> </Routes>
+                    <Routes> <Route path='/testing-we-love-kits-v46-24fps' element={landingKitsRender()} /> </Routes>
                 </div>
             </div>
 
