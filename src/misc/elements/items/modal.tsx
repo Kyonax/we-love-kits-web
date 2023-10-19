884bd67bcd089e5deb8b597a377bff90ee4badc3
Kyonax <kyonax.corp@gmail.com>
2023-10-19 07:24:00 -0500

    New Modals Adding System

---
+++ src/misc/elements/items/modal.tsx
@@ -0,0 +1,15 @@
+import {useEffect, useRef} from "react"
+import { LazyLoadImage } from 'react-lazy-load-image-component';
+
+import {motion, useInView, useAnimation, useIsPresent} from "framer-motion";
+
+interface ModalSectionProps {}
+
+const ModalSection: React.FC<ModalSectionProps> = ({}) => {
+
+    return (
+        <div></div>
+    )
+}
+
+export default ModalSection
